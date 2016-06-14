a8d99c960bc897bb33217ea60cd117b49f1b331b
John G. Wilson <jgwilson1997@gmail.com>
2016-06-14 11:48:05 -0400

    Added Actuator and Proximity testing .inos

---
+++ Actuator/actuator.ino
@@ -0,0 +1,45 @@
+  int stand = 24;
+  int PWMA = 7;
+  int AIN1 = 26;
+  int AIN2 = 28;
+  
+void setup() {
+  pinMode (stand, OUTPUT);
+  pinMode (PWMA, OUTPUT);
+  pinMode (AIN1, OUTPUT);
+  pinMode (AIN2, OUTPUT);
+
+}
+
+void loop() {
+  movement (1,255,1);
+  delay(5000);
+  movement (1,255,0);
+  delay(5000);
+  stopga();
+}
+
+void movement (int motor, int spd, int dir){
+
+  digitalWrite(stand, HIGH);
+
+  boolean inPin1 = LOW;
+  boolean inPin2 = HIGH;
+
+  if (dir == 1){
+    inPin1 = HIGH;
+    inPin2 = LOW;
+}
+
+  else{
+    inPin1 = LOW;
+    inPin2 = HIGH;
+  }
+  if (motor == 1){
+      digitalWrite(AIN1, inPin1);
+      digitalWrite(AIN2, inPin2);
+      analogWrite (PWMA, spd);
+    }
+}
+
+void stopga(){digitalWrite(stand, LOW);}
---
+++ Proximity/proximity.ino
@@ -0,0 +1,17 @@
+void setup() {
+  #define Proximity_sensor A3
+  pinMode(Proximity_sensor, INPUT);
+  Serial.begin(9600);
+  Serial.print("Test run");
+  while(true){
+      int value = analogRead(Proximity_sensor);
+      Serial.print("Proximity Sensor Data Value at:");
+      Serial.println(value);
+      delay(100);
+  }
+}
+
+void loop() {
+}
+
+
